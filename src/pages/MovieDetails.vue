675125879bccc3fc073d7b17db77878326d95cc1
juansefdz <jsfm81091@gmail.com>
2024-08-20 18:52:21 -0500

    update moviedetails

--- src/pages/MovieDetails.vue
+++ src/pages/MovieDetails.vue
@@ -1,54 +1,38 @@
 <template>
-  <div class="movie-details-container" v-if="movie">
-    <button class="button-home">HOME</button>
-    <div class="movies-container">
-      <div class="pic-container">
-        <img :src="movie.Poster" :alt="movie.Title" />
-      </div>
-      <div class="movies-details">
-        <h1>{{ movie.Title }}</h1>
-        <p>
-          <span>Year:</span>
-          {{ movie.Year }}
-        </p>
-        <p>
-          <span>Released:</span>
-          {{ movie.Released }}
-        </p>
-        <p>
-          <span>Director:</span>
-          {{ movie.Director }}
-        </p>
-        <p>
-          <span>Actors:</span>
-          {{ movie.Actors }}
-        </p>
-        <p>
-          <span>Language:</span>
-          {{ movie.Language }}
-        </p>
-        <p>
-          <span>Awards:</span>
-          {{ movie.Awards }}
-        </p>
-        <p>
-          <span>Metascore:</span>
-          {{ movie.Metascore }}
-        </p>
-        <p>
-          <span>IMDb Rating:</span>
-          {{ movie.imdbRating }}
-        </p>
+  <div class="movie-details-wrapper">
+    <button class="button-home" @click="goHome">HOME</button>
+    <div class="movie-details-container" v-if="movie">
+      <div class="movies-container">
+        <div class="pic-container">
+          <img :src="movie.Poster" :alt="movie.Title" />
+        </div>
+        <div class="movies-details">
+          <h1>{{ movie.Title }}</h1>
+          <p><span>Year:</span> {{ movie.Year }}</p>
+          <p><span>Released:</span> {{ movie.Released }}</p>
+          <p><span>Director:</span> {{ movie.Director }}</p>
+          <p><span>Actors:</span> {{ movie.Actors }}</p>
+          <p><span>Language:</span> {{ movie.Language }}</p>
+          <p><span>Awards:</span> {{ movie.Awards }}</p>
+          <p><span>Metascore:</span> {{ movie.Metascore }}</p>
+          <p><span>IMDb Rating:</span> {{ movie.imdbRating }}</p>
+          <p><span>IMDb Votes:</span> {{ movie.imdbVotes }}</p>
+          <p><span>Plot:</span> {{ movie.Plot }}</p>
+          
+        </div>
       </div>
     </div>
   </div>
 </template>
 
+
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import api from "@/api/client-http";
+
 const route = useRoute();
+const router = useRouter();
 const movieId = route.params.id as string;
 
 const movie = ref<any>(null);
@@ -66,35 +50,19 @@ const fetchMovieData = async () => {
   }
 };
 
+
+const goHome = () => {
+  router.push("/");
+};
+
 onMounted(() => {
   fetchMovieData();
 });
 </script>
 
+
 <style lang="scss" scoped>
-.movies-container {
-  display: flex;
-  flex-direction: row;
-  position: relative;
-  gap: 20px;
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-  box-sizing: border-box;
-}
-.button-home {
-  position: absolute;
-  top: 20px;
-  left: 20px;
-  background-color: $primary;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-  border-radius: 10px;
-  box-shadow: 0 0 10px 5px $primary;
-}
+
 
 .movie-details-container {
   width: 100vw;
@@ -122,8 +90,9 @@ onMounted(() => {
     img {
       max-width: 100%;
       border-radius: 10px;
-      border: solid 1px #e3e3e381;
-      box-shadow: 5px 7px 25px 2px $white;
+      box-shadow: 0px 0px 50px 20px $white;
+      border: solid 7px #ffffffbd;
+      animation: efectShine s infinite
     
     }
   }
@@ -159,4 +128,52 @@ onMounted(() => {
     }
   }
 }
+.movies-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 20px;
+  width: 100%;
+  height: 90%;
+  padding: 20px;
+  
+}
+.button-home {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  background-color: $primary;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 10px;
+  box-shadow: 0 0 10px 5px $primary;
+}
+.movie-details-wrapper {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.button-home {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  background-color: $primary;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 10px;
+  box-shadow: 0 0 10px 5px $primary;
+}
+
+
+
 </style>
